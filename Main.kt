0961f49e4ce8a8ce9775897a893b0529b2608451
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-02-08 21:53:47 +0300

    add files

---
+++ Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+   var fullName= "Cynthia Mueni"
+    println(fullName)
+
+    var age= 20
+    println(age)
+
+  var phoneNumber="0702553057"
+  println(phoneNumber)
+
+  var weight =85.5F
+  println(weight)
+
+  var kenyan = true
+  println(kenyan)
+
+
+
+
+
+}
